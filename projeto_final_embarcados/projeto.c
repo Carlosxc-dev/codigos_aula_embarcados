f93f4e2f63afb3fde4cf2a96645caa00ca9fd03b
Carlos Henrique <chcruz64@gmail.com>
2023-05-31 15:20:25 -0300

    up

--- projeto_final_embarcados/projeto.c
+++ projeto_final_embarcados/projeto.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "projeto.h"
-int start=0, end=0;
 
 // ler os dados do arquivo input.txt
 void ler_arquivo(Processo *p)
@@ -9,7 +8,8 @@ void ler_arquivo(Processo *p)
     FILE *f = fopen("input.txt", "r");
     Processo aux;
 
-    if (f == NULL) return;
+    if (f == NULL)
+        return;
 
     if ((end + 1) % BUFFER_SIZE == start)
     {
@@ -27,27 +27,27 @@ void ler_arquivo(Processo *p)
 }
 
 // imprime a saida do programa no arquivo output.txt
-void imprimir_saida(Processo *p)
+void imprimir_saida(Processo *p, char *text)
 {
     FILE *o = fopen("output.txt", "w");
-
+    fprintf(o, "%s \n", text);
     for (int i = 0; i < BUFFER_SIZE; i++)
     {
-        fprintf(o, "p[%d] %d %d \n", p[i].data, p[i].duracao, p[i].prioridade);
-        printf("%d ", p[i].data);
+        fprintf(o, "\tp[%d] %d %d \n", p[i].data, p[i].duracao, p[i].prioridade);
     }
-    
+
     // fechando aquivo de saida output.txt
     fclose(o);
 }
 
-//Priority-based Scheduling
-void schedule_priority(Processo *p){
-    
+// Priority-based Scheduling
+void schedule_priority(Processo *p)
+{
+
     Processo aux;
     for (int i = 0; i < BUFFER_SIZE; i++)
     {
-        for (int j = i+1; j < BUFFER_SIZE; j++)
+        for (int j = i + 1; j < BUFFER_SIZE; j++)
         {
             if (p[i].prioridade < p[j].prioridade)
             {
@@ -57,22 +57,40 @@ void schedule_priority(Processo *p){
             }
         }
     }
+}
+
+// Scheduling Multiple queues with feedback
+void schedule_multiple_queues(Processo *p)
+{
     
-    
+}
+
+void print_process(Processo *p)
+{
+    for (int i = 0; i < BUFFER_SIZE; i++)
+    {
+        printf("\tp[%d] %d %d \n", p[i].data, p[i].duracao, p[i].prioridade);
+    }
 }
 
 int main()
 {
-    Processo buffer_proc[BUFFER_SIZE];
 
     ler_arquivo(buffer_proc);
-    
+    printf("processos sem escalonamento\n");
+    print_process(buffer_proc);
+
+    printf("processos escalonados priority\n");
     schedule_priority(buffer_proc);
+    print_process(buffer_proc);
 
-    imprimir_saida(buffer_proc);
-    
+    printf("processos escalonados multiple queues\n");
+    schedule_multiple_queues(buffer_proc);
+    print_process(buffer_proc);
 
+    printf("\nstart = %d, end = %d\n", end, start);
+
+    imprimir_saida(buffer_proc, "processos escalonado");
 
     return 0;
-
 }
--- projeto_final_embarcados/projeto.h
+++ projeto_final_embarcados/projeto.h
@@ -1,22 +1,26 @@
 #ifndef PROJETO_H
 #define PROJETO_H
 
-#define BUFFER_SIZE 10
+#define BUFFER_SIZE 5
 
 // armazena todos os dados pertinentes de cada processo
 typedef struct Processo
 {
     int data;       // data de criação
     int duracao;    // tempo de execução
-    int prioridade; //proridade
+    int prioridade; // proridade
 } Processo;
 
-
+// O número de processos presentes no arquivo de entrada deverá ter
+// no mínimo o dobro do tamanho do buffer circular.
+Processo buffer_proc[BUFFER_SIZE];
+int start = 0, end = 0;
+int clock_tick = 0; // controle do quantum
 
 void ler_arquivo(Processo *p);
-void imprimir_saida(Processo *p);
-
-
-
+void imprimir_saida(Processo *p, char *text);
+void schedule_priority(Processo *p);
+void schedule_multiple_queues(Processo *p);
+void print_process(Processo *p);
 
 #endif
